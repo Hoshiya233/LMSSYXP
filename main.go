165a4432ca3449963ad23f24d899a400644f599c
hoshiya233 <hoshiya233@outlook.com>
2022-02-13 19:25:00 +0800

    发现了在linux中挂载媒体目录的bug，暂时以写死地方式解决

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"strings"
-
 	"example.com/m/v2/controller"
 	"example.com/m/v2/tool"
 	"github.com/gin-gonic/gin"
@@ -17,15 +15,19 @@ func main() {
 	xpManager.Static("/static", "./static")
 
 	//挂载媒体目录
-	var path_head_list []string
-	for _, path := range config.MMDpaths {
-		a := strings.Split(path, ":")[0]
-		path_head_list = append(path_head_list, a)
-	}
-	path_head_list = tool.DeleteRepeatList(path_head_list)
-	for _, a := range path_head_list {
-		xpManager.Static("/"+a, a+":\\")
-	}
+	// var path_head_list []string
+	// for _, path := range config.MMDpaths {
+	// 	a := strings.Split(path, ":")[0]
+	// 	path_head_list = append(path_head_list, a)
+	// }
+	// path_head_list = tool.DeleteRepeatList(path_head_list)
+	// for _, a := range path_head_list {
+	// 	xpManager.Static("/"+a, a+":\\")
+	// }
+	// 以上适用windows系统，把盘符挂载进来
+
+	// bug: 在linux中，如果挂载的目录有空格，会报404
+	xpManager.Static("/mnt", "/mnt")
 
 	registerRouter(xpManager)
 
